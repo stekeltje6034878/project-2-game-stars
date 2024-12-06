d32f24d65117f7638528861c5bcfbdfd118e3bbb
thijs <thijs.stekeltje@gmail.com>
2024-12-06 12:05:16 +0100

    volledig klaar, nu is het ook daatwerkelijk klaar

--- pages/game_view.php
+++ pages/game_view.php
@@ -6,42 +6,46 @@
     <meta name="description" content="Voeg hier een beschrijving van je pagina toe.">
     <meta name="keywords" content="sleutelwoord1, sleutelwoord2, sleutelwoord3">
     <meta name="author" content="Thijs Stekelenburg">
-    <link rel="stylesheet", href="../css/stylesheet.css">
+    <link rel="stylesheet" href="../css/stylesheet.css">
     <title>easy_backend</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
     <?php
-
-    $games = array(
-        'id1' => array(
+    // Associatieve array met game info
+    $games = [
+        'id1' => [
             'name' => 'God Of War Ragnarok',
-            'img' => '../assets/god-of-war-ragnarok.avif'
-        ),
-        'id2' => array(
+            'img' => '../assets/god-of-war-ragnarok.avif',
+        ],
+        'id2' => [
             'name' => 'Stardew Valley',
-            'img' => '../assets/Stardew-Valley.png'
-        ),
-        'id3' => array(
-            'name' => 'Assasins Creed Valhalla',
-            'img' => '../assets/assasins-creed-valhalla.avif'
-        )
-    );
+            'img' => '../assets/Stardew-Valley.png',
+        ],
+        'id3' => [
+            'name' => 'The Witcher 3: Wild Hunt',
+            'img' => '../assets/witcher3.png',
+        ],
+    ];
 
-    $selectedgame = 'id2';
+    // Variabele om een game te selecteren
+    $selectedgame = 'id2'; // Verander deze waarde om een andere game te tonen
 
+    // Game-info ophalen op basis van geselecteerde game
     if (array_key_exists($selectedgame, $games)) {
         $name = $games[$selectedgame]['name'];
         $img = $games[$selectedgame]['img'];
+    } else {
+        $name = 'Game niet gevonden';
+        $img = '../assets/default.png';
     }
     ?>
 </head>
 
 <body>
 <header id="reviewheader">
-    <h1 class="gameName"><?php echo $name ?></h1>
+    <h1 class="gameName"><?= $name ?></h1>
 </header>
 <section>
-    <img id="reviewImg" src= <?php echo $img ?> ''>
+    <img id="reviewImg" src="<?= $img ?>" alt="Afbeelding van <?= $name ?>">
 </section>
 </body>
-
 </html>
